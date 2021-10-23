aeb0d5b4f43f891b2016b6d53ebd079893638199
Ioana Ramona Martin <martinramona50@yahoo.ro>
2021-10-23 16:44:02 +0300

    Finished lab4

---
+++ Students/Martin Ioana Ramona/LAB4/src/ro/model/Product.java
@@ -0,0 +1,48 @@
+package ro.model;
+
+public class Product {
+    private String name;
+    private int quantity;
+    private Category category;
+    private float price;
+
+    public Product(String name, int quantity, Category category, float price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.category = category;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+}
